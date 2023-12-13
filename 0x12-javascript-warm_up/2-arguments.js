f85db8ba719874d4d980f32cc7f7a8bf4c3fc498
Mwaikingson <133366179+Mwaikingson@users.noreply.github.com>
2023-12-13 21:48:01 +0300

    0x12-javascript-warm_up
    For alx-higher_level_programming

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const index = process.argv.length;
+if (index === 2) {
+  console.log('No argument');
+} else if (index === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
